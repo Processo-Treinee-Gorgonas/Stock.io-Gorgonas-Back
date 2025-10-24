b4fa5f0a882011e55fd82b744469f20754f1001e
dev-americo <dev.americo@gmail.com>
2025-10-24 19:28:46 -0300

    feat: criacao dos modulos necessarios e database

---
+++ backend/src/usuario/usuario.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUsuarioDto } from './dto/create-usuario.dto';
+import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { PrismaService } from '../database/prisma.service';
+
+@Injectable()
+export class UsuarioService {
+}
