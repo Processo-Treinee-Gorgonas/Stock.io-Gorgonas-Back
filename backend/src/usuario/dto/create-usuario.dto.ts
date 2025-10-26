9c145b32ba033acaadb4b2f8d1fab5ebe0cc7b1b
dev-americo <dev.americo@gmail.com>
2025-10-26 10:06:33 -0300

    feat: criacao de dto create-usuario

--- backend/src/usuario/dto/create-usuario.dto.ts
+++ backend/src/usuario/dto/create-usuario.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUsuarioDto {}
+import { IsString, IsNotEmpty, IsInt, IsOptional, IsBoolean, MinLength, Matches, IsEmail } from 'class-validator';
+
+export class CreateUsuarioDto {
+
+    @IsEmail()
+    @IsNotEmpty()
+    userName: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    nome: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    email: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
+    senha: string;
+
+    @IsString()
+    @IsNotEmpty()
+    confirmarSenha: string;
+    
+    @IsString()
+    @IsOptional()
+    fotoPerfil?: string;
+}
